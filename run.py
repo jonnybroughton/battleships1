097ca5748190dec44a4c7c4ef1045b483362ddea
jonny broughton <jonny.broughton08@gmail.com>
2024-05-20 12:09:53 +0000

    Add description docstrings to current functions

--- run.py
+++ run.py
@@ -1,12 +1,22 @@
 import random
-def create_bpard(size):
+def create_board(size):
+    """
+    Create the game board
+    """
     return [["~"] * size for i in range(size)]
 
 def print_board(board):
+    """
+    Print the current game board
+    """
     for row in board:
         print(" ".join(row))
 
 def place_ships(board, size):
+    """
+    Place ships randomly on the game board
+    """
     ship_row = random.randint(0, size - 1)
     ship_col = random.randint(0, size - 1)
     return ship_row, ship_col
+
