bbe7a99dbd0b69d3dd0b93f151e6c084811cc6ac
jonny broughton <jonny.broughton08@gmail.com>
2024-05-20 11:55:52 +0000

    Make function to place battleships randomly on the board

--- run.py
+++ run.py
@@ -5,3 +5,8 @@ def create_bpard(size):
 def print_board(board):
     for row in board:
         print(" ".join(row))
+
+def place_ships(board, size):
+    ship_row = random.randint(0, size - 1)
+    ship_col = random.randint(0, size - 1)
+    return ship_row, ship_col
